ae5f9162056622d2320869ec5c2fe0136080ce6a
benginio <jeanbaptistebenginio@gmail.com>
2022-08-12 22:17:19 -0700

    first commit to my app twitter with code path

---
+++ app/src/main/java/com/codepath/apps/CodePatherTweets/TmelineActivity.java
@@ -0,0 +1,127 @@
+package com.codepath.apps.CodePatherTweets;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.apps.CodePatherTweets.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class TmelineActivity extends AppCompatActivity {
+    public static final String TAG = "TimelineActivity";
+    EndlessRecyclerViewScrollListener scrollListener;
+    TwitterClient client;
+    RecyclerView rvTweets;
+    List<Tweet> tweets;
+    TweetsAdapter adapter;
+    SwipeRefreshLayout swipeContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tmeline);
+        client = TwitterApp.getRestClient(this);
+        swipeContainer = findViewById(R.id.swipeConotainer);
+
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i(TAG,"fetching new data");
+                populateHomeTimeline();
+            }
+        });
+        //Find the recycle view
+        rvTweets =findViewById(R.id.rvTweets);
+        //Init the list of tweets and a adapter
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(this, tweets);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        //Recycler view setup: layout manager  and the adapter
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                Log.i(TAG,"onLoadMore" +page);
+                loadMoreData();
+
+            }
+        };
+
+        // Adds the scroll listener to RecyclerView
+        rvTweets.addOnScrollListener(scrollListener);
+        populateHomeTimeline();
+    }
+
+    private void loadMoreData() {
+        // 1. Send an API request to retrieve appropriate paginated data
+        client.getNextPageOfTweets(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                // 2. Deserialize and construct new model objects from the API response
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    List<Tweet> tweets= Tweet.fromJsonArray(jsonArray);
+                    // 3. Append the new data objects to the existing set of items inside the array of items
+                    // 4. Notify the adapter of the new items made with `notifyItemRangeInserted()
+                    adapter.addAll(tweets);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG,"onFailure for loadMoreData!", throwable);
+            }
+        },  tweets.get(tweets.size() -1 ).id);
+
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG,"onSuccess!"+json.toString());
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    //List<Tweet> tweets=Tweet.fromJsonArray(jsonArray);
+                    adapter.clear();
+                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                    adapter.notifyDataSetChanged();
+                    //Now we call setRefreshing(false) to signal refresh has finished
+                    swipeContainer.setRefreshing(false);
+
+                } catch (JSONException e) {
+                    Log.e(TAG,"Json exception",e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG,"onFailure!"+response, throwable);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/CodePatherTweets/models/Tweet.java
@@ -0,0 +1,37 @@
+package com.codepath.apps.CodePatherTweets.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tweet {
+    public long id;
+    public String body;
+    public String createdAt;
+    public User user;
+    private static JSONObject jsonObject;
+
+
+    //Empty constructor needed by to Parceler Library
+    public Tweet(){}
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("text");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.id =jsonObject.getLong("id");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        return tweet;
+
+    }
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for(int i=0; i < jsonArray.length(); i++){
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return  tweets;
+    }
+}
